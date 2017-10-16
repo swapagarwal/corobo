850bd9fc7e9db3dcb7cae9c0173857b2f5d55b30
Swapnil Agarwal <swapagarwal@users.noreply.github.com>
2017-10-16 21:40:09 +0000

    ship_it: Add more squirrel images

--- plugins/ship_it.py
+++ plugins/ship_it.py
@@ -11,10 +11,13 @@ class Ship_it(BotPlugin):
 
     # start ignoring LineLengthBear PyCodeStyleBear
     IMAGES = [
-        'http://i.imgur.com/DPVM1.png',
-        'http://d2f8dzk2mhcqts.cloudfront.net/0772_PEW_Roundup/09_Squirrel.jpg',
-        'http://www.cybersalt.org/images/funnypictures/s/supersquirrel.jpg',
-        'http://www.zmescience.com/wp-content/uploads/2010/09/squirrel.jpg',
+        'https://i.imgur.com/DPVM1.png',
+        'https://d2f8dzk2mhcqts.cloudfront.net/0772_PEW_Roundup/09_Squirrel.jpg',
+        'https://www.cybersalt.org/images/funnypictures/s/supersquirrel.jpg',
+        'https://www.zmescience.com/wp-content/uploads/2010/09/squirrel.jpg',
+        'https://qph.ec.quoracdn.net/main-qimg-c8781a4bb1f17e330b50cb35f851da05-c',
+        'https://shipitsquirrel.github.io/images/squirrel.png',
+        'https://1.bp.blogspot.com/_v0neUj-VDa4/TFBEbqFQcII/AAAAAAAAFBU/E8kPNmF1h1E/s640/squirrelbacca-thumb.jpg',
     ]
     # stop ignoring LineLengthBear PyCodeStyleBear
 
